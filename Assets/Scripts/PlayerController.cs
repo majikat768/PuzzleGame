137193455aeb4f3cfa5fecabea3bdb590fe5b1a7
Michael <atki7828@users.noreply.github.com>
2020-10-10 18:32:58 -0700

    Added simple player movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        this.transform.position += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        
+    }
+}
